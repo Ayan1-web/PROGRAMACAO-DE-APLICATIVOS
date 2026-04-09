d5b696f3bfe5e6d38a6302552c76bf8dda0a47a1
Ayan1-web <ayancesar@gmail.com>
2026-04-09 16:52:00 -0300

    Add ReprodutorMusical interface with music controls

---
+++ 09_04_26/ReprodutorMusical.java
@@ -0,0 +1,16 @@
+public interface ReprodutorMusical{
+    
+    public void tocar();
+        
+    public void pausar();
+
+    public void selecionarMusica(String musica);
+
+    public String getMusica();
+    
+    public void setMusica(String musica);
+
+    public String getNome();
+
+    public void setNome(String nome);
+}

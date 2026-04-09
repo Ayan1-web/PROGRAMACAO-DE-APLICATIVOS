7fb23b3f950f6e917478863de583b89467eecdee
Ayan1-web <ayancesar@gmail.com>
2026-04-09 16:52:50 -0300

    Create AparelhoTelefonico.java

---
+++ 09_04_26/AparelhoTelefonico.java
@@ -0,0 +1,18 @@
+public interface AparelhoTelefonico{
+
+    public void ligar();
+
+    public void atender();
+
+    public void correioDeVoz();
+
+    public String getNumero();
+
+    public void setNumero(String numero);
+
+    public String getModelo();
+
+    public void setModelo(String modelo);
+
+    
+}
